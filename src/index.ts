05a9eeb224f0d20c5908170fec184eef27159aa6
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-06-04 17:00:09 +0200

    Version 1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,53 @@
-import { moduleHelloWorld } from "./modules/module-helper.js";
-import { utilityHelloWorld } from "./utility/utility-helper.js";
+import Color, { color } from "./modules/color.js";
 
-moduleHelloWorld();
-utilityHelloWorld();
+console.log(color("This is reset text", Color.Reset));
+console.log(color("This is black text", Color.Black));
+console.log(color("This is red text", Color.Red));
+console.log(color("This is green text", Color.Green));
+console.log(color("This is yellow text", Color.Yellow));
+console.log(color("This is blue text", Color.Blue));
+console.log(color("This is magenta text", Color.Magenta));
+console.log(color("This is cyan text", Color.Cyan));
+console.log(color("This is white text", Color.White));
+console.log(color("This is black background text", Color.BlackBackground));
+console.log(color("This is red background text", Color.RedBackground));
+console.log(color("This is green background text", Color.GreenBackground));
+console.log(color("This is yellow background text", Color.YellowBackground));
+console.log(color("This is blue background text", Color.BlueBackground));
+console.log(color("This is magenta background text", Color.MagentaBackground));
+console.log(color("This is cyan background text", Color.CyanBackground));
+console.log(color("This is white background text", Color.WhiteBackground));
+console.log(color("This is bright black text", Color.BrightBlack));
+console.log(color("This is bright red text", Color.BrightRed));
+console.log(color("This is bright green text", Color.BrightGreen));
+console.log(color("This is bright yellow text", Color.BrightYellow));
+console.log(color("This is bright blue text", Color.BrightBlue));
+console.log(color("This is bright magenta text", Color.BrightMagenta));
+console.log(color("This is bright cyan text", Color.BrightCyan));
+console.log(color("This is bright white text", Color.BrightWhite));
+console.log(
+  color("This is bright black background text", Color.BrightBlackBackground)
+);
+console.log(
+  color("This is bright red background text", Color.BrightRedBackground)
+);
+console.log(
+  color("This is bright green background text", Color.BrightGreenBackground)
+);
+console.log(
+  color("This is bright yellow background text", Color.BrightYellowBackground)
+);
+console.log(
+  color("This is bright blue background text", Color.BrightBlueBackground)
+);
+console.log(
+  color("This is bright magenta background text", Color.BrightMagentaBackground)
+);
+console.log(
+  color("This is bright cyan background text", Color.BrightCyanBackground)
+);
+console.log(
+  color("This is bright white background text", Color.BrightWhiteBackground)
+);
+console.log(color("This is orange text", Color.Orange));
+console.log(color("This is bright orange text", Color.BrightOrange));
